6fb3041d169c52b8e44d030e8fa041711ea4b5b2
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2025-06-05 22:43:05 -0300

    feat: add nestjs as framework

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["node_modules/", "coverage/"]),
+  globalIgnores(["node_modules/", "coverage/", "dist/"]),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
